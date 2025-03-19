940a36b2b42081d00b003a5f1286f4314da59323
4l00ps <matgits@icloud.com>
2025-03-19 21:53:35 +0200

    Initial commit: Set up Spring Boot QR Auth Demo

---
+++ src/main/java/com/muhammadali/qrauth/qr_auth_demo/QrCodeAuthenticationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.muhammadali.qrauth.qr_auth_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrCodeAuthenticationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrCodeAuthenticationDemoApplication.class, args);
+	}
+
+}
